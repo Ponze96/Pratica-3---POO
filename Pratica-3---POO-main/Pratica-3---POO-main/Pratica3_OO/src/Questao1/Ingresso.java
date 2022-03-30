5f47dcc484e76bff554ed6a481de62f30c1406ca
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-03-29 22:30:36 -0300

    Add files via upload

---
+++ Pratica-3---POO-main/Pratica-3---POO-main/Pratica3_OO/src/Questao1/Ingresso.java
@@ -0,0 +1,47 @@
+package Questao1;
+
+public class Ingresso {
+		private float valor;
+
+		public Ingresso(float valor) {
+			
+			this.valor = valor;
+		}
+			
+			
+		 }
+			
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Pratica-3---POO-main/Pratica-3---POO-main/Pratica3_OO/src/Questao1/Normal.java
@@ -0,0 +1,16 @@
+package Questao1;
+
+public class Normal extends Ingresso {
+	
+	Normal(float valor){
+		super(valor);
+	}
+	
+	public void imprimeingressoNormal(){
+        System.out.println("Ingresso Normal:"+ getValorIngresso() );
+       
+    }
+}
+	
+	
+
