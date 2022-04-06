aeb6b567014772003cecc6ed015c67abecfe99f6
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-04-05 22:26:47 -0300

    Add files via upload

---
+++ Pratica-3---POO-main/Pratica-3---POO-main/Pratica3_OO/src/Questao2/Empresa.java
@@ -0,0 +1,14 @@
+package Questao2;
+
+public class Empresa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FPadrao f1 = new Fpadrao (123, "Joao", 1000);
+		System.out.println(f1.toString());
+		
+		FComissionado f2 = new Fcomissionado(%, "Maria", 1000, 500, 10);
+		System.out.println(f2.toString());
+	}
+
+}
